faf7c3e41110fc1c07dd6bd0dcf59be1c9434a3e
kaya5091 <21511091yk@tama.ac.jp>
2017-05-30 10:33:01 +0900

    Transactional確認

--- src/main/java/tama/ac/jp/AccessDbApplication.java
+++ src/main/java/tama/ac/jp/AccessDbApplication.java
@@ -6,24 +6,28 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tama.ac.jp.dao.PasswordDAO;
 import tama.ac.jp.dao.UserDAO;
+import tama.ac.jp.service.UserService;
 
 @SpringBootApplication
 public class AccessDbApplication implements CommandLineRunner
 {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccessDbApplication.class, args);
-	}
-
-//	@Autowired
-//	UserDAO userDao;
+	@Autowired
+	UserDAO userDao;
 
 	@Autowired
 	PasswordDAO passwordDAO;
 
+	@Autowired
+	UserService userService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessDbApplication.class, args);
+	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		userService.multiUserRegister();
 	}
 
 }
---
+++ src/main/java/tama/ac/jp/service/UserService.java
@@ -0,0 +1,26 @@
+package tama.ac.jp.service;
+
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import tama.ac.jp.dao.UserDAO;
+import tama.ac.jp.model.User;
+
+/**
+ * Created by YK on 2017/05/30.
+ */
+
+@Service
+public class UserService {
+    @Autowired
+    private UserDAO userDAO;
+
+//    @Transactional
+    public void multiUserRegister(){
+        val user1 = new User("20611082","小澤遊矢");
+        userDAO.insertUser(user1);
+        val badRequestUser = new User("21511091","茅島佑樹");
+        userDAO.insertUser(badRequestUser);
+    }
+}
